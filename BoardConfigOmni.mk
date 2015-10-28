62cbc0c4518fc7d32ceb5af55217612d506e645c
erikcas <erikcas1972@gmail.com>
2015-10-28 20:37:21 +0100

    custom kernel
    Change-Id: Idccfa8639433c99e1bedc6bf3c9c35a769a9f6df

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BOARD_CUSTOM_BOOTIMG := true
+
 # TWRP
 BOARD_HAS_NO_SELECT_BUTTON := true
 
